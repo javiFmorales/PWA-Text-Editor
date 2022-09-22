97c12bb10be334506ff4958e8544018564b49c0a
javiermorales <javiertoto-00@hotmail.com>
2022-09-22 11:00:12 -0700

    get db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,21 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {console.error('putDb not implemented');
 const jatetDb = await openDB('jate', 1);
   const tx =jatetDb .transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ content: content });
+  const request = store.put({content: content });
   const result = await request;
   console.log(' data saved to the database', result);
-
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-const contactDb = await openDB('contact', 1);
-  // const tx = contactDb.transaction('contact', 'readwrite');
-  // const store = tx.objectStore('contact');
-  // const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
-  // const result = await request;
-  // console.log('🚀 - data saved to the database', result);
-
+export const getDb = async () => {
+console.error('getDb not implemented');
+const jatetDb = await openDB('jate', 1);
+const tx =jatetDb .transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result = await request;
+console.log(' data saved to the database', result);
+}
 initdb();
