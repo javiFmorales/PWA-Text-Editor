0a774042a2fc519d92fac93c1a155ae74a03e7f9
javiermorales <javiertoto-00@hotmail.com>
2022-09-22 11:44:34 -0700

    db updates

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {console.error('putDb not implemented');
+export const putDb = async (content) => {console.error('putDb implemented');
 const jatetDb = await openDB('jate', 1);
   const tx =jatetDb .transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -23,7 +23,7 @@ const jatetDb = await openDB('jate', 1);
 }
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-console.error('getDb not implemented');
+console.error('getDb implemented');
 const jatetDb = await openDB('jate', 1);
 const tx =jatetDb .transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
