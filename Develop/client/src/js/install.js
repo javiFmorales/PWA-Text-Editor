75382eb9a712e8ce0d5cc3d898d5ea7a0308181d
javiermorales <javiertoto-00@hotmail.com>
2022-09-22 11:59:25 -0700

    cache

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -6,6 +6,8 @@ window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
 });
 
+
+
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
@@ -17,6 +19,8 @@ butInstall.addEventListener('click', async () => {
 
     window.deferredPrompt = null;
 });
+
+
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
